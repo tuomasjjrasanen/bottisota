6bdc0d8e8fed5f0b40d5feee484ca02ee287cae8
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2018-07-01 19:17:04 +0000

    math: add minmaxloc()

--- bottisota/math.py
+++ bottisota/math.py
@@ -1,5 +1,7 @@
 import math
 
+import bottisota.constants
+
 def direction(point0, point1):
     x0, y0 = point0
     x1, y1 = point1
@@ -9,6 +11,14 @@ def distance(point0, point1):
     x0, y0 = point0
     x1, y1 = point1
     return math.hypot(x1 - x0, y1 - y0)
+
+def minmaxloc(loc):
+    x, y = loc
+
+    return (
+        min(bottisota.constants.ARENA_X_MAX, max(bottisota.constants.ARENA_X_MIN, x)),
+        min(bottisota.constants.ARENA_Y_MAX, max(bottisota.constants.ARENA_Y_MIN, y)),
+    )
 
 def travel(x, y, direction, distance):
     xd = math.cos(math.radians(direction)) * distance
