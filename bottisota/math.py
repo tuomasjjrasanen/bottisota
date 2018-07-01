e473af929177300cff7a0933a3a19f141b917b17
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2018-07-01 19:19:04 +0000

    math: add functions for float return values

--- bottisota/math.py
+++ bottisota/math.py
@@ -2,15 +2,21 @@ import math
 
 import bottisota.constants
 
-def direction(point0, point1):
+def direction_f(point0, point1):
     x0, y0 = point0
     x1, y1 = point1
-    return round(math.degrees(math.atan2(y1 - y0, x1 - x0))) % 360
+    return math.degrees(math.atan2(y1 - y0, x1 - x0)) % 360
 
-def distance(point0, point1):
+def direction(point0, point1):
+    return round(direction_f(point0, point1))
+
+def distance_f(point0, point1):
     x0, y0 = point0
     x1, y1 = point1
     return math.hypot(x1 - x0, y1 - y0)
+
+def distance(point0, point1):
+    return round(distance_f(point0, point1))
 
 def minmaxloc(loc):
     x, y = loc
